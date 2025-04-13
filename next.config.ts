b8df39cf0715ea5a4ce3607a61bc43f3814ac36c
Mamdouh <mamdouh.mohammed919@gmail.com>
2025-04-13 12:58:06 +0200

    updates

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/api/file",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "false" },
+          { key: "Access-Control-Allow-Origin", value: "*" }, // Dynamic origin
+          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date" },
+        ]
+      }
+    ]
+  },
   images: {
     domains: ["firebasestorage.googleapis.com"], // Add your image domain here
   },
